276eb3d24b9606283873bcf3211373fa6bd75f1a
Hobson37 <120533345+Hobson37@users.noreply.github.com>
2022-12-20 12:55:48 -0600

    Delete SourceCode/Inventory Manager/Login directory

--- SourceCode/Inventory Manager/Login/Partials/app-TopNav.php
+++
@@ -1,4 +0,0 @@
-	<div class="dashboardTopNav">
-		<a href="" id="toggleBtn"><i class="fa fa-navicon"></i></a>
-		<a href="./database/logout.php" id="logoutBtn"><i class="fa fa-power-off"></i>Log Out</a>
-	</div>
